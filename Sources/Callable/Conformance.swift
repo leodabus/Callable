317c489f308915b589c6a0eb4df40145819bca8b
ScottLydon <scottlydon18@gmail.com>
2022-07-12 21:37:10 -0700

    Add DownloadTask session

--- Sources/Callable/Conformance.swift
+++ Sources/Callable/Conformance.swift
@@ -14,7 +14,41 @@ extension URL: Callable {
     }
 }
 
+public typealias DownloadCompletion = (URL?, URLResponse?, Error?) -> Void
+
+extension URL: ProvidesSessionDownloadDataTask {
+    
+    public func session(_ downloadCompletion: @escaping DownloadCompletion) -> URLSessionDownloadTask {
+        URLSession.shared.downloadTask(with: self, completionHandler: downloadCompletion)
+    }
+    
+    private func sessionDownloadTask(provideData: DataAction?) -> URLSessionDownloadTask {
+        session { url, response, error in
+            guard let data = url?.data else {
+                errorPrint()
+                provideData?("error: \(error?.localizedDescription ?? "nil")".data(using: .utf8)!)
+                return
+            }
+            provideData?(data)
+        }
+    }
+    
+    private func errorPrint() {
+        print("ERROR: data was nil for the call from: \(self), ")
+    }
+}
+
+
 extension URL {
+    
+    var data: Data?  {
+        do {
+            return try Data(contentsOf: self, options: .mappedIfSafe)
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
     
     public func callLocalCodable<T: Codable>(expressive: Bool = false, _ action: (T?)->Void) throws {
         action(try Data(contentsOf: self, options: .mappedIfSafe).codable())
