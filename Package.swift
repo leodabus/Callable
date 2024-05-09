5b787b7fcd2aff135f5331d512130152dbca94ee
Leonardo Savio Dabus <leodabus@gmail.com>
2024-05-09 05:57:47 -0300

    add url

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // .package(url: "https://github.com/leodabus/Callable.git", from: "2.0.0"),
         
     ],
     targets: [
